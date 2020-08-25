c71f1f31d9756280758ca5ed96dd75188456939f
Łukasz Chruściel <lchrusciel@gmail.com>
2020-08-25 10:50:29 +0200

    Sending an email with state machine

---
+++ src/Sender/TrustedSupplierEmailSender.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Sender;
+
+use App\Entity\Supplier;
+use Sylius\Component\Mailer\Sender\SenderInterface;
+
+final class TrustedSupplierEmailSender
+{
+    /**
+     * @var SenderInterface
+     */
+    private $sender;
+
+    public function __construct(SenderInterface $sender)
+    {
+        $this->sender = $sender;
+    }
+
+    public function send(Supplier $supplier): void
+    {
+        $this->sender->send('trust_supplier', [$supplier->getEmail()], ['supplier' => $supplier]);
+    }
+}
