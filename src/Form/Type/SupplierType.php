53d77ccdbed2df34fe4c3a7d8e060c4cfa574204
Łukasz Chruściel <lchrusciel@gmail.com>
2020-08-25 11:12:35 +0200

    Overriding default form type

---
+++ src/Form/Type/SupplierType.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Form\Type;
+
+use Sylius\Bundle\ResourceBundle\Form\Type\AbstractResourceType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+final class SupplierType extends AbstractResourceType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class)
+            ->add('email', EmailType::class)
+        ;
+    }
+}
